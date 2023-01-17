1a456566c5b26c8438f3f3a3499dc8ffd35e026c
jcordeirocg <jcordeiro@compugraf.com.br>
2023-01-17 19:13:45 -0300

    Rename from JsonObjects to jDocument

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='jDocument',
     packages=find_packages(include=['jDocument']),
     version='0.1.0',
-    description='The jDocument class allows you to encapsulate a json document (dict or a list) and perform a lot of operations to read, update and add data',
+    long_description=long_description,
+    long_description_content_type='text/plain',
     author='Jose Carlos Cordeiro Martins',
     license='MIT',
     setup_requires=['Unidecode~=1.3.6', 'python-dateutil~=2.8.2'],
